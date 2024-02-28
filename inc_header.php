ee6b0c0fa47207c4cb9d910234a06fce566f7899
Pethum229 <pathumpp99@gmail.com>
2024-02-28 15:48:48 +0530

    Navigation Menu Updated

---
+++ inc_header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Library Management System</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <!-- Header HTML  <-Start-> -->
+    <header>
+        <div class="navSec">
+            <ul class="navigation">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="">Book Inventory</a></li>
+                <li><a href="">About Us</a></li>
+                <li><a href="">Contact Us</a></li>
+            </ul>
+            <button class="login"><a href="">Login <i class="fa-solid fa-right-to-bracket"></i></a></button>
+        </div>
+    </header>
+    <!-- Header HTML  <-End-> -->
--- index.php
+++ index.php
@@ -1,24 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Library Management System</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <!-- Header HTML  <-Start-> -->
-    <header>
-        <div>
-            <ul class="navigation">
-                <li><a href="index.php">Home</a></li>
-                <li><a href="">Book Inventory</a></li>
-                <li><a href="">About Us</a></li>
-                <li><a href="">Contact Us</a></li>
-            </ul>
-            <button><a href="">Login</a></button>
-        </div>
-    </header>
-    <!-- Header HTML  <-End-> -->
+<?php
+    include "inc_header.php";
+?>
+
+    <section>
+
+    </section>
+    
 </body>
 </html>
